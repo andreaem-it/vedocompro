a1e2f646dac7d34a69ef19120aa4dc61dd9d4288
James Clark <james.clark@sky.uk>
2017-11-14 21:36:47 +0000

    Moved to FOS User Bundle

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,75 +1,51 @@
 <?php
+
 namespace AppBundle\Entity;
 
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\AdvancedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
- * @ORM\Entity
+ * @Entity
  * @ORM\Table(name="users")
  */
-class User implements UserInterface, \Serializable
+class User extends BaseUser
 {
     /**
-     * @ORM\Column(type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-    
+    protected $id;
+
     /**
      * @ORM\Column(type="string", length=100)
      */
     private $name;
-    
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $email;
-    
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $password;
-    
+
     /**
      * @ORM\Column(type="date")
      */
     private $datejoin;
-    
-    /**
-     * @ORM\Column(name="is_active", type="boolean")
-     */
-    private $isActive;
-    
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $address;
-    
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $points;
-    /**
-     * @ORM\Column(type="string")
-     * @ORM\ManyToMany(targetEntity="Role", inversedBy="users")
-     */
-    private $roles;
-    
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $phone;
 
     /**
      * @ORM\Column(type="string")
      */
-    private $username;
+    private $address;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $points;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $phone;
 
     /**
      * @ORM\Column(type="string")
@@ -101,6 +77,7 @@ class User implements UserInterface, \Serializable
      */
 
     private $credits_silver;
+
     /**
      * @ORM\Column(type="integer")
      */
@@ -109,155 +86,18 @@ class User implements UserInterface, \Serializable
     /**
      * @ORM\Column(type="text")
      */
-    private $confirmation_token;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $passwordRequestedAt;
-
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $plainPassword;
-
-
-    public function getSalt()
-    {
-        return null;
-    }
+    protected $plainPassword;
 
     public function __construct()
     {
-        $this->role = new ArrayCollection();
-    }
-
-    public function eraseCredentials()
-    {
-    }
-
-    public function isAccountNonExpired()
-    {
-        return true;
-    }
-
-    public function isAccountNonLocked()
-    {
-        return true;
-    }
-
-    public function isCredentialsNonExpired()
-    {
-        return true;
-    }
-
-    public function isEnabled()
-    {
-        return $this->isActive;
-    }
-
-    public function serialize()
-    {
-        return serialize(array(
-            $this->id,
-            $this->isActive
-        ));
-    }
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->isActive
-        ) = unserialize($serialized);
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return User
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return mixed
-     */
-
-    public function getPassword()
-    {
-        return $this->password;
+        parent::__construct();
+        // your own logic
     }
 
     /**
      * Set datejoin
      *
-     * @param DateTime $datejoin
+     * @param \DateTime $datejoin
      *
      * @return User
      */
@@ -271,35 +111,11 @@ class User implements UserInterface, \Serializable
     /**
      * Get datejoin
      *
-     * @return DateTime
+     * @return \DateTime
      */
     public function getDatejoin()
     {
         return $this->datejoin;
-    }
-
-    /**
-     * Set isActive
-     *
-     * @param boolean $isActive
-     *
-     * @return User
-     */
-    public function setIsActive($isActive)
-    {
-        $this->isActive = $isActive;
-
-        return $this;
-    }
-
-    /**
-     * Get isActive
-     *
-     * @return boolean
-     */
-    public function getIsActive()
-    {
-        return $this->isActive;
     }
 
     /**
@@ -351,30 +167,6 @@ class User implements UserInterface, \Serializable
     }
 
     /**
-     * Get role
-     *
-     * @return array
-     */
-    public function getRoles()
-    {
-        return array($this->roles);
-    }
-
-    /**
-     * Set role
-     *
-     * @param string $role
-     *
-     * @return User
-     */
-    public function setRoles($roles)
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
      * Set phone
      *
      * @param string $phone
@@ -396,31 +188,6 @@ class User implements UserInterface, \Serializable
     public function getPhone()
     {
         return $this->phone;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     *
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return mixed
-     */
-
-    public function getUsername()
-    {
-        return $this->username;
     }
 
     /**
@@ -591,127 +358,27 @@ class User implements UserInterface, \Serializable
         return $this->credits_bronze;
     }
 
-    public function uploadFile()
-    {
-        // the file property can be empty if the field is not required
-        if (null === $this->getFile()) {
-            return;
-        }
-
-        // use the original file name here but you should
-        // sanitize it at least to avoid any security issues
-
-        // move takes the target directory and then the
-        // target filename to move to
-        $this->getFile()->move($this->getUploadDir(), $this->getFile()->getClientOriginalName());
-
-        // set the path property to the filename where you've saved the file
-        $this->path = $this->getFile()->getClientOriginalName();
-
-        // clean up the file property as you won't need it anymore
-        $this->file = null;
-    }
-
     /**
-     * Set confirmationToken
+     * Set name
      *
-     * @param string $confirmationToken
+     * @param string $name
      *
      * @return User
      */
-    public function setConfirmationToken($confirmationToken)
+    public function setName($name)
     {
-        $this->confirmation_token = $confirmationToken;
+        $this->name = $name;
 
         return $this;
     }
 
     /**
-     * Get confirmationToken
+     * Get name
      *
      * @return string
      */
-    public function getConfirmationToken()
+    public function getName()
     {
-        return $this->confirmation_token;
-    }
-
-    /**
-     * Set passwordRequestedAt
-     *
-     * @param \DateTime $passwordRequestedAt
-     *
-     * @return User
-     */
-    public function setPasswordRequestedAt($passwordRequestedAt)
-    {
-        $this->passwordRequestedAt = $passwordRequestedAt;
-
-        return $this;
-    }
-
-    /**
-     * Get passwordRequestedAt
-     *
-     * @return \DateTime
-     */
-    public function getPasswordRequestedAt()
-    {
-        return $this->passwordRequestedAt;
-    }
-
-    /**
-     * Set plainPassword
-     *
-     * @param string $plainPassword
-     *
-     * @return User
-     */
-    public function setPlainPassword($plainPassword)
-    {
-        $this->plainPassword = $plainPassword;
-
-        return $this;
-    }
-
-    /**
-     * Get plainPassword
-     *
-     * @return string
-     */
-    public function getPlainPassword()
-    {
-        return $this->plainPassword;
-    }
-
-    /**
-     * Set rePassword
-     *
-     * @param string $rePassword
-     *
-     * @return User
-     */
-    public function setRePassword($rePassword)
-    {
-        $this->plainPassword = $rePassword;
-
-        return $this;
-    }
-
-    /**
-     * Get rePassword
-     *
-     * @return string
-     */
-    public function getRePassword()
-    {
-        return $this->plainPassword;
-    }
-
-    public function __toString() {
-        if(is_null($this->email)) {
-            return 'NULL';
-        }
-        return $this->email;
+        return $this->name;
     }
 }
