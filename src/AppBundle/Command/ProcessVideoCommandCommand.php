04966f7a903228a01a1548ebdecb172c666c5537
James Clark <james.clark@sky.uk>
2017-11-24 08:35:38 +0000

    Added error catching

--- src/AppBundle/Command/ProcessVideoCommandCommand.php
+++ src/AppBundle/Command/ProcessVideoCommandCommand.php
@@ -24,6 +24,7 @@ class ProcessVideoCommandCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $logger = $this->getContainer()->get('logger');
         $videos = $em->getRepository('AppBundle:Videos')->findBy(['uploaded' => false]);
         $raw_dir = __DIR__ . "/../../../web/webtemp/rawvideos/";
         $newvideo_dir = __DIR__ . "/../../../web/webtemp/encvideos/";
@@ -37,36 +38,45 @@ class ProcessVideoCommandCommand extends ContainerAwareCommand
         /** @var Videos $video */
         foreach ($videos as $video) {
             if ($video->getFilename() != "") {
-                $new_video_name = $video->getAid() . ".mp4";
-                $new_video_path = $newvideo_dir . $new_video_name;
-                $new_image_name = $video->getAid() . ".jpg";
-                $new_image_path = $newphoto_dir . $new_image_name;
-                $cmd = sprintf(
-                    'ffmpeg -y -i %s -vf "scale=-2:720:flags=lanczos" -vcodec libx264 -profile:v main -level 3.1 -preset medium -crf 23 -x264-params ref=4 -acodec copy -movflags +faststart %s > /dev/null 2>&1',
-                    $raw_dir . $video->getFilename(),
-                    $new_video_path
-                );
-                exec($cmd);
+                try {
+                    $new_video_name = $video->getAid() . ".mp4";
+                    $new_video_path = $newvideo_dir . $new_video_name;
+                    $new_image_name = $video->getAid() . ".jpg";
+                    $new_image_path = $newphoto_dir . $new_image_name;
+                    $cmd = sprintf(
+                        'ffmpeg -y -i %s -vf "scale=-2:720:flags=lanczos" -vcodec libx264 -profile:v main -level 3.1 -preset medium -crf 23 -x264-params ref=4 -acodec copy -movflags +faststart %s > /dev/null 2>&1',
+                        $raw_dir . $video->getFilename(),
+                        $new_video_path
+                    );
+                    exec($cmd);
 
-                // TODO - don't hardcode 4 seconds as if the video is shorter, this won't work
-                $cmd = sprintf(
-                    'ffmpeg -i %s -an -ss 00:00:04 -r 1 -vframes 1 -f mjpeg -y %s > /dev/null 2>&1',
-                    $raw_dir . $video->getFilename(),
-                    $new_image_path
-                );
-                exec($cmd);
+                    // TODO - don't hardcode 4 seconds as if the video is shorter, this won't work
+                    $cmd = sprintf(
+                        'ffmpeg -i %s -an -ss 00:00:04 -r 1 -vframes 1 -f mjpeg -y %s > /dev/null 2>&1',
+                        $raw_dir . $video->getFilename(),
+                        $new_image_path
+                    );
+                    exec($cmd);
 
-                // Upload to S3
-                $fs_video->write($new_video_name, file_get_contents($new_video_path));
-                $fs_photo->write($new_image_name, file_get_contents($new_image_path));
+                    // Upload to S3
+                    $fs_video->write($new_video_name, file_get_contents($new_video_path));
+                    $fs_photo->write($new_image_name, file_get_contents($new_image_path));
 
-                // TODO Delete old file
+                    // TODO Delete old file
 
-                $s3_video_link = "https://s3.eu-west-2.amazonaws.com/vedocompro/video/" . $fs_video->get($new_video_name)->getName();
-                $video->setFilename($s3_video_link);
-                $video->setUploaded(true);
-                $em->persist($video);
-                $em->flush();
+                    $s3_video_link = "https://s3.eu-west-2.amazonaws.com/vedocompro/video/" . $fs_video->get($new_video_name)->getName();
+                    $video->setFilename($s3_video_link);
+                    $video->setUploaded(true);
+                    $em->persist($video);
+                    $em->flush();
+                } catch (\Exception $e) {
+                    $logger->error(
+                        sprintf("Failed to process video for ad ID %d: %s\n",
+                            $video->getAid(),
+                            $e->getMessage()
+                        )
+                    );
+                }
             }
         }
     }
