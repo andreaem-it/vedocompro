dbc0ca387cc73d423c22eb4b1432532938a27947
Andrea Emili <andreyodj@gmail.com>
2019-12-05 17:50:59 +0100

    added PWA service-worker

---
+++ web/service-worker.js
@@ -0,0 +1,38 @@
+importScripts('app/cache-polyfill.js');
+
+self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+
+    event.waitUntil(
+        caches.open('vedocompro').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/app.php',
+                '/css/bootstrap.min.css',
+                '/css/base.css',
+                '/css/custom.css',
+                '/js/bootstrap.min.js',
+                '/js/jquery-3.3.1.min.js'
+            ]);
+        })
+    );
+
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+    event.respondWith(
+        caches.open('mysite-dynamic').then(function(cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
